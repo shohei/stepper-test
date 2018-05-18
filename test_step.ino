82f6f1731f428762fd6104f8498ac7bfd7d0bc53
Shohei Aoki <shoaok@gmail.com>
2018-05-18 09:50:42 +0900

    update

--- test_step.ino
+++ test_step.ino
@@ -1,17 +1,19 @@
 #define PPS       1000
 #define MICROSTEP 16
-#define DELAY_US (int)(1000*1000/(PPS*MICROSTEP))
+// #define DELAY_US (int)(1000*1000/(PPS*MICROSTEP))
+#define DELAY_US 1000
 
 #define X_STEP_PIN 18 
 #define X_DIR_PIN 42 
 #define X_EN_PIN 12 
 
 #define TRAVEL_S 3
-#define TOTAL (int)((TRAVEL_S*1000*1000)/DELAY_US)
+#define TOTAL 300000
 
 #define PULSE_DELAY_US 2
 
 void setup(){
+  Serial.begin(9600);
   pinMode(X_STEP_PIN,OUTPUT);
   pinMode(X_DIR_PIN,OUTPUT);
   pinMode(X_EN_PIN,OUTPUT);
@@ -20,13 +22,16 @@ void setup(){
 }
 
 void loop(){
+  Serial.println("CW");
   digitalWrite(X_DIR_PIN,HIGH);
-  for(int i=0;i<TOTAL;i++){
+  for(long i=0;i<TOTAL;i++){
   	oneshot();
   	delayMicroseconds(DELAY_US);
   }
+
+  Serial.println("CCW");
   digitalWrite(X_DIR_PIN,LOW);
-  for(int i=0;i<TOTAL;i++){
+  for(long i=0;i<TOTAL;i++){
   	oneshot();
   	delayMicroseconds(DELAY_US);
   }
@@ -34,7 +39,7 @@ void loop(){
 
 void oneshot(){
  	digitalWrite(X_STEP_PIN,HIGH);
-  	delayMicroseconds(PULSE_DELAY_US);
-  	digitalWrite(X_STEP_PIN,LOW);
-  	delayMicroseconds(PULSE_DELAY_US);
+  delayMicroseconds(PULSE_DELAY_US);
+  digitalWrite(X_STEP_PIN,LOW);
+  delayMicroseconds(PULSE_DELAY_US);
 }

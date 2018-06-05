2238415bb83668ab90e5099fba30510aa694b7af
Shohei Aoki <shoaok@gmail.com>
2018-06-06 06:51:29 +0900

    update

--- stepper-test.ino
+++ stepper-test.ino
@@ -1,31 +1,31 @@
 #define PPS       1000
 #define MICROSTEP 16
 // #define DELAY_US (int)(1000*1000/(PPS*MICROSTEP))
-#define DELAY_US 1000
+#define DELAY_US 10000
 
 #define X1_STEP_PIN    18 
 #define X1_DIR_PIN     42 
 #define X1_EN_PIN      12 
-#define X2_STEP_PIN    36 
-#define X2_DIR_PIN     19 
-#define X2_EN_PIN      49 
-#define Y1_STEP_PIN    48 
-#define Y1_DIR_PIN     33 
+//#define X2_STEP_PIN    36 
+//#define X2_DIR_PIN     19 
+//#define X2_EN_PIN      49 
+#define Y1_STEP_PIN    38
+#define Y1_DIR_PIN     33
 #define Y1_EN_PIN      48 
-#define Y2_STEP_PIN    37 
-#define Y2_DIR_PIN     41 
-#define Y2_EN_PIN      45 
+//#define Y2_STEP_PIN    37 
+//#define Y2_DIR_PIN     41 
+//#define Y2_EN_PIN      45 
 #define Z1_STEP_PIN    34 
 #define Z1_DIR_PIN     40 
 #define Z1_EN_PIN      47 
-#define Z2_STEP_PIN    31 
-#define Z2_DIR_PIN     35 
-#define Z2_EN_PIN      46 
+//#define Z2_STEP_PIN    31 
+//#define Z2_DIR_PIN     35 
+//#define Z2_EN_PIN      46 
 #define E_STEP_PIN     30 
 #define E_DIR_PIN      32 
 #define E_EN_PIN       43
 #define A_STEP_PIN     65 
-#define A_DIR_PIN      64 
+#define A_DIR_PIN      66 
 #define A_EN_PIN       44 
 
 #define TRAVEL_S 3
@@ -38,21 +38,21 @@ void setup(){
   pinMode(X1_STEP_PIN,OUTPUT);
   pinMode(X1_DIR_PIN, OUTPUT);
   pinMode(X1_EN_PIN,  OUTPUT);
-  pinMode(X2_STEP_PIN,INPUT);
-  pinMode(X2_DIR_PIN, INPUT);
-  pinMode(X2_EN_PIN,  INPUT);
+//  pinMode(X2_STEP_PIN,INPUT);
+//  pinMode(X2_DIR_PIN, INPUT);
+//  pinMode(X2_EN_PIN,  INPUT);
   pinMode(Y1_STEP_PIN,OUTPUT);
   pinMode(Y1_DIR_PIN, OUTPUT);
   pinMode(Y1_EN_PIN,  OUTPUT);
-  pinMode(Y2_STEP_PIN,INPUT);
-  pinMode(Y2_DIR_PIN, INPUT);
-  pinMode(Y2_EN_PIN,  INPUT);
+//  pinMode(Y2_STEP_PIN,INPUT);
+//  pinMode(Y2_DIR_PIN, INPUT);
+//  pinMode(Y2_EN_PIN,  INPUT);
   pinMode(Z1_STEP_PIN,OUTPUT);
   pinMode(Z1_DIR_PIN, OUTPUT);
   pinMode(Z1_EN_PIN,  OUTPUT);
-  pinMode(Z2_STEP_PIN,INPUT);
-  pinMode(Z2_DIR_PIN, INPUT);
-  pinMode(Z2_EN_PIN,  INPUT);
+//  pinMode(Z2_STEP_PIN,INPUT);
+//  pinMode(Z2_DIR_PIN, INPUT);
+//  pinMode(Z2_EN_PIN,  INPUT);
   pinMode(E_STEP_PIN, OUTPUT);
   pinMode(E_DIR_PIN,  OUTPUT);
   pinMode(E_EN_PIN,   OUTPUT);
